c73391a2c63ed09d1cdd817bd90cc21a3e5f2499
julesdmai <julesdmai@gmail.com>
2024-08-07 15:27:17 -0700

    added comments in email reminder Lambda function

--- Serverless/email_reminder_lambda.py
+++ Serverless/email_reminder_lambda.py
@@ -7,7 +7,8 @@ ses = boto3.client('ses')
 def lambda_handler(event, context):
     # Print event data to logs .. 
     print("Received event: " + json.dumps(event))
-    # Publish message directly to email, provided by EmailOnly or EmailPar TASK
+
+    # Publish message directly to email, provided by EmailOnly or EmailPar TASK (email parameter or through a task)
     ses.send_email( Source=FROM_EMAIL_ADDRESS,
         Destination={ 'ToAddresses': [ event['Input']['email'] ] }, 
         Message={ 'Subject': {'Data': 'Whiskers Commands You to attend!'},
